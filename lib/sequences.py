565a94296553501731050917ad58c4e7d548bd84
cole-code1 <collinskathu313@gmail.com>
2024-12-01 07:06:28 +0300

    initial commit

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+
+	seq = []
+	if length > 0:
+		seq.append(0)
+	if length >= 2:
+		seq.append(1)
+		for i in range(2, length):
+			seq.append(seq[-1] + seq[-2])
+
+	print(seq)
+
+print_fibonacci(1)
